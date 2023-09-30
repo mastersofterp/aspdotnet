4a747c077e1f1116919504f8698c14a5cb83661a
sonal banode <sonal.banode@mastersofterp.com>
2023-09-30 12:31:22 +0530

    [CHERRY-PICK] Adding 'b20c2af31ee19f12ac64c68b7c5e61a629b2d1f2' to 'CHERRY_PICK_UAT_2023-10-12_17.49.31' using the python script version 'v1.0.5'.

--- PresentationLayer/Health/Transaction/DirectMedicineIssue.aspx.cs
+++ PresentationLayer/Health/Transaction/DirectMedicineIssue.aspx.cs
@@ -1757,7 +1757,8 @@ public partial class Health_Transaction_DirectMedicineIssue : System.Web.UI.Page
             objD.MAC_ADDRESS = "";
             objD.COLLEGE_CODE = Session["colcode"].ToString();
 
-            string[] pNAME = txtPatientName.Text.Split('*');
+           // string[] pNAME = txtPatientName.Text.Split('*');
+            string[] pNAME = txtPatientName.Text.Split('-');
             string[] dNAME = ddlDependent.SelectedItem.Text.Split('-');
             if (lblPatientCat.Text == "E")
             {
@@ -1768,11 +1769,11 @@ public partial class Health_Transaction_DirectMedicineIssue : System.Web.UI.Page
                     //objD.PATIENT_NAME = pNAME[1];
                     if (pNAME.Length == 1)
                     {
-                        objHelTran.PATIENT_NAME = pNAME[0];
+                        objD.PATIENT_NAME = pNAME[0];
                     }
                     else
                     {
-                        objHelTran.PATIENT_NAME = pNAME[1];
+                        objD.PATIENT_NAME = pNAME[1];
                     }
                 }
                 else
