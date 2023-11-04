93e30d37b5b2d9543ca68591da8887516dcb2456
Shrikant Waghmare <shrikant.waghmare@mastersofterp.com>
2023-11-04 14:11:53 +0530

    [CHERRY-PICK] Adding '3918cb0393e269947fe50f18fa3f4ab986e47c26' to 'CHERRY_PICK_main_2023-11-27_15.43.23' using the python script version 'v1.0.5'.

--- PresentationLayer/SiteMasterPage.master.cs
+++ PresentationLayer/SiteMasterPage.master.cs
@@ -3239,48 +3239,51 @@ public partial class SiteMasterPage : System.Web.UI.MasterPage
 
 
     protected DataSet GetPdfPath()  // Added By Anurag B. on 31-10-2023
-    { 
+    {
         string currentPageName = Request.Url.Segments[Request.Url.Segments.Length - 1];
-        //string AL_No = Request.QueryString["pageno"].ToString().Trim();
         DataSet ds = new DataSet();
-        //DataSet ds = null;
+        int AL_No = 0;
+        AL_No = Convert.ToInt32(Request.QueryString["pageno"]);
         try
         {
-            // SQLHelper objDataAccess = new SQLHelper(System.Configuration.ConfigurationManager.ConnectionStrings["UAIMS"].ConnectionString);
-            //SqlParameter[] sqlParams = new SqlParameter[]
-            //{
-            //    new SqlParameter("@P_AL_URL", currentPageName)
-            //};
-            //ds = objDataAccess.ExecuteDataSetSP("PKG_GET_PDF_NAME", sqlParams);
             if (currentPageName == "principalHome.aspx" || currentPageName == "homeFaculty.aspx" || currentPageName == "homeNonFaculty.aspx" || currentPageName == "studeHome.aspx" || currentPageName == "Links.aspx")
             {
                 ds = objCommon.FillDropDown("ACCESS_LINK", "AL_Link", "MANUAL_UPLOAD_FILENAME", "AL_URL='" + currentPageName + "' ", "");
             }
             else
             {
-                string AL_No = Request.QueryString["pageno"].ToString().Trim();
-                ds = objCommon.FillDropDown("ACCESS_LINK", "AL_Link", "MANUAL_UPLOAD_FILENAME", "AL_No='" + AL_No + "' ", "");
+                if (AL_No != 0)
+                {
+                    ds = objCommon.FillDropDown("ACCESS_LINK", "AL_Link", "MANUAL_UPLOAD_FILENAME", "AL_No='" + AL_No + "' ", "");
+                }
+                else
+                {
+                    AL_No = 0;
+                }
             }
         }
         catch (Exception ex)
         {
         }
-        if (ds != null && ds.Tables[0].Rows.Count > 0 )
+        if (AL_No != 0)
         {
-            string path = ds.Tables[0].Rows[0]["MANUAL_UPLOAD_FILENAME"].ToString();
-            string folderpath = Server.MapPath("~/UserManual/") + path; // Use Server.MapPath to get the physical path
-            if (!File.Exists(folderpath))
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
             {
-                //pdfViewer.Src = "UserManual/Help Document Not.pdf" + "#toolbar=0&navpanes=0&scrollbar=0";
-                string textToDisplay = "<div style='text-align: center; font-size: 24px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);'>Help Information for " + ds.Tables[0].Rows[0]["AL_Link"].ToString() + " Not Found.</div>";
-                pdfViewer.Attributes["srcdoc"] = textToDisplay;
-                btndownload.Visible = false;
+                string path = ds.Tables[0].Rows[0]["MANUAL_UPLOAD_FILENAME"].ToString();
+                string folderpath = Server.MapPath("~/UserManual/") + path; // Use Server.MapPath to get the physical path
+                if (!File.Exists(folderpath))
+                {
+                    //pdfViewer.Src = "UserManual/Help Document Not.pdf" + "#toolbar=0&navpanes=0&scrollbar=0";
+                    string textToDisplay = "<div style='text-align: center; font-size: 24px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);'>Help Information for " + ds.Tables[0].Rows[0]["AL_Link"].ToString() + " Not Found.</div>";
+                    pdfViewer.Attributes["srcdoc"] = textToDisplay;
+                    btndownload.Visible = false;
+                }
+                else
+                {
+                    pdfViewer.Src = ResolveUrl("~/UserManual/") + path + "#toolbar=0&navpanes=0&scrollbar=0";
+                }
+                informationid.InnerText = "Help - " + ds.Tables[0].Rows[0]["AL_Link"].ToString() + "";
             }
-            else
-            {
-                pdfViewer.Src = ResolveUrl("~/UserManual/") + path + "#toolbar=0&navpanes=0&scrollbar=0";
-            }
-            informationid.InnerText = "Help - " + ds.Tables[0].Rows[0]["AL_Link"].ToString() + "";
         }
         else
         {
