adf22f8d880ce119b8798ac472ed1b1d9cf06549
prashant wankar <prashant.wankar@mastersofterp.com>
2023-12-16 11:29:25 +0530

    [CHERRY-PICK] Adding '101783ae5f63082249d308f9dc7ff9172cab17de' to 'CHERRY_PICK_main_2024-03-14_15.44.12' using the python script version 'v1.0.8'.

--- PresentationLayer/PAYROLL/TRANSACTIONS/Pay_Attendance.aspx.cs
+++ PresentationLayer/PAYROLL/TRANSACTIONS/Pay_Attendance.aspx.cs
@@ -16,12 +16,22 @@ using IITMS;
 using IITMS.UAIMS;
 using IITMS.UAIMS.BusinessLayer.BusinessEntities;
 using IITMS.UAIMS.BusinessLayer.BusinessLogic;
+using System.IO;
+using ClosedXML.Excel;
+using System.Configuration;
+using System.Data.OleDb;
+
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
 {
     Common objCommon = new Common();
     UAIMS_Common objUCommon = new UAIMS_Common();
     AttendanceController objAttendance = new AttendanceController();
+    ExcelPayHeadImportController objEPIC = new ExcelPayHeadImportController();
 
     protected void Page_PreInit(object sender, EventArgs e)
     {
@@ -34,7 +44,7 @@ public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+
 
         CheckRef();
 
@@ -57,9 +67,9 @@ public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
                 //Load Page Help
                 if (Request.QueryString["pageno"] != null)
                 {
-                   // lblHelp.Text = objCommon.GetPageHelp(int.Parse(Request.QueryString["pageno"].ToString()));
+                    // lblHelp.Text = objCommon.GetPageHelp(int.Parse(Request.QueryString["pageno"].ToString()));
                 }
-                pnlSelect.Visible = true;                
+                pnlSelect.Visible = true;
                 pnlAttendance.Visible = false;
                 pnlNote.Visible = false;
                 FillDropdown();
@@ -100,17 +110,17 @@ public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
         int orderByIdno;
         //if (!(Convert.ToInt32(index) == 0))
         //{
-            if (chkAbsent.Checked)
-                absent = 1;
-            else
-                absent = 0;
+        if (chkAbsent.Checked)
+            absent = 1;
+        else
+            absent = 0;
 
-            if (chkIdno.Checked)
-                orderByIdno = 1;
-            else
-                orderByIdno = 0;
+        if (chkIdno.Checked)
+            orderByIdno = 1;
+        else
+            orderByIdno = 0;
 
-            this.BindListViewList(Convert.ToInt32(ddlStaff.SelectedValue.ToString()), absent, Convert.ToInt32(ddlorderby.SelectedValue), Convert.ToInt32(ddlDepartment.SelectedValue.ToString()), Convert.ToInt32(ddlCollege.SelectedValue.ToString()));
+        this.BindListViewList(Convert.ToInt32(ddlStaff.SelectedValue.ToString()), absent, Convert.ToInt32(ddlorderby.SelectedValue), Convert.ToInt32(ddlDepartment.SelectedValue.ToString()), Convert.ToInt32(ddlCollege.SelectedValue.ToString()));
         //}
         //else
         //{
@@ -118,36 +128,37 @@ public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
         //}
 
     }
-    
-    private void BindListViewList(int staffno, int showAbsent, int orderByIdno,int Dept, int collegeNo)
+
+    private void BindListViewList(int staffno, int showAbsent, int orderByIdno, int Dept, int collegeNo)
     {
-       
+
         try
         {
             //if (!(Convert.ToInt32(ddlStaff.SelectedIndex) == 0))
             //{
 
-                pnlAttendance.Visible = true;
-                DataSet ds = objAttendance.GetAttendanceOfEmployee(staffno, showAbsent, orderByIdno, Dept, Convert.ToInt32(ddlEmployeeType.SelectedValue) ,collegeNo);
-                if (ds.Tables[0].Rows.Count <= 0)
-                {
-                    
-                    pnlNote.Visible = false;
-                    btnSave.Visible = false;
-                    btnCancel.Visible = false;
-                }
-                else
-                {
-                    
-                  //  lblcount.Text = ds.Tables[0].Rows.Count.ToString();
-                    
-                    pnlNote.Visible = true;
-                    btnSave.Visible = true;
-                    btnCancel.Visible = true;
-                }
-                
-                lvAttendance.DataSource = ds;
-                lvAttendance.DataBind();
+            pnlAttendance.Visible = true;
+            DataSet ds = objAttendance.GetAttendanceOfEmployee(staffno, showAbsent, orderByIdno, Dept, Convert.ToInt32(ddlEmployeeType.SelectedValue), collegeNo);
+
+            if (ds.Tables[0].Rows.Count <= 0)
+            {
+
+                pnlNote.Visible = false;
+                btnSave.Visible = false;
+                btnCancel.Visible = false;
+            }
+            else
+            {
+
+                //  lblcount.Text = ds.Tables[0].Rows.Count.ToString();
+
+                pnlNote.Visible = true;
+                btnSave.Visible = true;
+                btnCancel.Visible = true;
+            }
+
+            lvAttendance.DataSource = ds;
+            lvAttendance.DataBind();
             //}
 
         }
@@ -170,7 +181,7 @@ public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
         {
         }
     }
-    
+
     protected void btnSub_Click(object sender, EventArgs e)
     {
         try
@@ -300,7 +311,7 @@ public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
 
 
         //this.BindListViewList(Convert.ToInt32(ddlStaff.SelectedValue.ToString()), absent, Convert.ToInt32(ddlorderby.SelectedValue), Convert.ToInt32(ddlDepartment.SelectedValue.ToString()), Convert.ToInt32(ddlCollege.SelectedValue.ToString()));
-        
+
 
     }
 
@@ -346,4 +357,259 @@ public partial class PayRoll_Pay_Attendance : System.Web.UI.Page
                 objUCommon.ShowError(Page, "Server UnAvailable");
         }
     }
+
+
+
+    private void ExcelReport()
+    {
+
+        grdSelectFieldReport.DataSource = null;
+        grdSelectFieldReport.DataBind();
+
+        DataSet ds = objAttendance.GetAttendanceOfEmployee(Convert.ToInt32(ddlStaff.SelectedValue.ToString()), Convert.ToInt32(chkAbsent.Checked), Convert.ToInt32(chkIdno.Checked), Convert.ToInt32(ddlDepartment.SelectedValue.ToString()), Convert.ToInt32(ddlEmployeeType.SelectedValue), Convert.ToInt32(ddlCollege.SelectedValue.ToString()));
+
+
+        if (ds != null && ds.Tables[0].Rows.Count > 0)
+        {
+            ds.Tables[0].TableName = "AttendanceEntry";
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                foreach (System.Data.DataTable dt in ds.Tables)
+                {
+                    //Add System.Data.DataTable as Worksheet.
+                    if (dt != null && dt.Rows.Count > 0)
+                    {
+                        wb.Worksheets.Add(dt);
+                        grdSelectFieldReport.DataSource = dt;
+                        grdSelectFieldReport.DataBind();
+                    }
+                    
+                }
+                wb.ShowGridLines.ToString();
+                //Export the Excel file.
+                Response.Clear();
+                Response.Buffer = true;
+                Response.Charset = "";
+
+                // Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.ContentType = "application/vnd.ms-excel";
+                Response.AddHeader("content-disposition", "attachment;filename=AttendanceEntry" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".xls");
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new HtmlTextWriter(sw);
+                grdSelectFieldReport.RenderControl(htw);
+                Response.Write(sw.ToString());
+                Response.End();
+
+
+                //using (MemoryStream MyMemoryStream = new MemoryStream())
+                //{
+
+                //    wb.SaveAs(MyMemoryStream);
+                //    MyMemoryStream.WriteTo(Response.OutputStream);
+                //    Response.Flush();
+                //    Response.End();
+                //}
+            }
+        }
+        else
+        {
+            objCommon.DisplayUserMessage(this.Page, "No Record Found", this.Page);
+            return;
+        }
+
+    }
+    protected void btnExport_Click(object sender, EventArgs e)
+    {
+        ExcelReport();
+    }
+
+    protected void imgExportCSV_Click(object sender, ImageClickEventArgs e)
+    {
+        ExcelReport();
+    }
+
+
+    protected void imgbutExporttoexcel_Click(object sender, ImageClickEventArgs e)
+    {
+        string folderPath = string.Empty;
+        string Path = string.Empty;
+        DataSet ds = new DataSet();
+        try
+        {
+            if (FileUpload2.HasFile)
+            {
+                string FileName = FileUpload2.FileName;
+                string ext = System.IO.Path.GetExtension(FileUpload2.FileName);
+                if (ext == ".xls" || ext == ".xlsx")
+                {
+                    CustomStatus cs = new CustomStatus();
+
+                    folderPath = Server.MapPath("~/Other Rem/");
+                    Path = "~/Other Rem/";
+                    if (!System.IO.Directory.Exists(folderPath))
+                    {
+                        System.IO.Directory.CreateDirectory(folderPath);
+                    }
+
+                    string path = string.Concat(Server.MapPath(Path + FileUpload2.FileName));
+
+
+                    FileUpload2.PostedFile.SaveAs(path);
+                    string fileName = FileUpload2.FileName;
+
+                    //  OleDbConnection OleDbcon = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;");
+
+                    OleDbConnection OleDbcon = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR={1}'");
+
+                    //OleDbCommand cmd = new OleDbCommand("SELECT * FROM ["+ddlSubPayhead.SelectedItem.Text+"$]", OleDbcon);
+
+                    OleDbCommand cmd = new OleDbCommand("SELECT * FROM [AttendanceEntry$]", OleDbcon);//Sheet1
+
+                    OleDbDataAdapter objAdapter1 = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    objAdapter1.Fill(ds);
+
+                    dt = ds.Tables[0];
+                    int i = 0;
+
+
+                    Payroll objPayroll = new Payroll();
+                    objPayroll.PAYHEAD = "PAYDAYS";
+
+
+                    for (i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        //  objPayroll.PFILENO = ds.Tables[0].Rows[i]["PFILENO"].ToString() == "" || ds.Tables[0].Rows[i]["PFILENO"].ToString() == null ? "0" : ds.Tables[0].Rows[i]["PFILENO"].ToString();
+                        // objPayroll.PFILENO = ds.Tables[0].Rows[i]["Employee Code"].ToString();
+
+                        if (ds.Tables[0].Rows[i]["Employee Code"].ToString() != "" || ds.Tables[0].Rows[i]["Employee Code"].ToString() != string.Empty)
+                        {
+                            objPayroll.PFILENO = ds.Tables[0].Rows[i]["Employee Code"].ToString();
+                            objPayroll.TOTAMT = ds.Tables[0].Rows[i]["Absent Days"] == "0.0" || ds.Tables[0].Rows[i]["Absent Days"] == DBNull.Value ? Convert.ToDecimal("0") : Convert.ToDecimal(ds.Tables[0].Rows[i]["Absent Days"]);
+
+                            cs = (CustomStatus)objEPIC.UpdatePayHeadsByExcel(objPayroll);
+                        }
+                    }
+                    if (cs.Equals(CustomStatus.RecordUpdated))
+                    {
+                        objCommon.DisplayMessage("Data Updated Successfully!!", this);
+                        //ddlPayhead.SelectedIndex = 0;
+                        return;
+                    }
+                }
+                else
+                {
+                    objCommon.DisplayMessage("Upload Only Excel Format!!", this);
+                    //ddlPayhead.SelectedIndex = 0;
+                    return;
+                }
+            }
+            else
+            {
+                objCommon.DisplayMessage("Please Select Excel File!!", this);
+                // ddlPayhead.SelectedIndex = 0;
+                return;
+            }
+        }
+
+        catch (Exception ex)
+        {
+            //objCommon.DisplayMessage("Exception Occured,Contact To Administrator!!", this);
+            objCommon.DisplayMessage("Data is not in correct format!!", this);
+        }
+    }
+
+    protected void btnImport_Click(object sender, EventArgs e)
+    {
+        string folderPath = string.Empty;
+        string Path = string.Empty;
+        DataSet ds = new DataSet();
+        try
+        {
+            if (FileUpload2.HasFile)
+            {
+                string FileName = FileUpload2.FileName;
+                string ext = System.IO.Path.GetExtension(FileUpload2.FileName);
+                if (ext == ".xls" || ext == ".xlsx")
+                {
+                    CustomStatus cs = new CustomStatus();
+                   
+                    folderPath = Server.MapPath("~/Other Rem/");
+                    Path = "~/Other Rem/";
+                    if (!System.IO.Directory.Exists(folderPath))
+                    {
+                        System.IO.Directory.CreateDirectory(folderPath);
+                    }
+                   
+                    string path = string.Concat(Server.MapPath(Path + FileUpload2.FileName));    
+             
+
+                    FileUpload2.PostedFile.SaveAs(path);
+                    string fileName = FileUpload2.FileName;
+
+                    //  OleDbConnection OleDbcon = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;");
+
+                    OleDbConnection OleDbcon = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR={1}'");
+
+                    //OleDbCommand cmd = new OleDbCommand("SELECT * FROM ["+ddlSubPayhead.SelectedItem.Text+"$]", OleDbcon);
+
+                    OleDbCommand cmd = new OleDbCommand("SELECT * FROM [AttendanceEntry$]", OleDbcon);//Sheet1
+
+                    OleDbDataAdapter objAdapter1 = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    objAdapter1.Fill(ds);
+
+                    dt = ds.Tables[0];
+                    int i = 0;
+
+
+                    Payroll objPayroll = new Payroll();
+                    objPayroll.PAYHEAD = "PAYDAYS";
+                 
+
+                    for (i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        //  objPayroll.PFILENO = ds.Tables[0].Rows[i]["PFILENO"].ToString() == "" || ds.Tables[0].Rows[i]["PFILENO"].ToString() == null ? "0" : ds.Tables[0].Rows[i]["PFILENO"].ToString();
+                       // objPayroll.PFILENO = ds.Tables[0].Rows[i]["Employee Code"].ToString();
+
+                        if (ds.Tables[0].Rows[i]["Employee Code"].ToString() != "" || ds.Tables[0].Rows[i]["Employee Code"].ToString() != string.Empty)
+                        {
+                            objPayroll.PFILENO = ds.Tables[0].Rows[i]["Employee Code"].ToString();
+                            objPayroll.TOTAMT = ds.Tables[0].Rows[i]["Absent Days"] == "0.0" || ds.Tables[0].Rows[i]["Absent Days"] == DBNull.Value ? Convert.ToDecimal("0") : Convert.ToDecimal(ds.Tables[0].Rows[i]["Absent Days"]);
+
+                            cs = (CustomStatus)objEPIC.UpdatePayHeadsByExcel(objPayroll);
+                        }
+                    }
+                    if (cs.Equals(CustomStatus.RecordUpdated))
+                    {
+                        objCommon.DisplayMessage("Data Updated Successfully!!", this);
+                        //ddlPayhead.SelectedIndex = 0;
+                        return;
+                    }
+                }
+                else
+                {
+                    objCommon.DisplayMessage("Upload Only Excel Format!!", this);
+                    //ddlPayhead.SelectedIndex = 0;
+                    return;
+                }
+            }
+            else
+            {
+                objCommon.DisplayMessage("Please Select Excel File!!", this);
+               // ddlPayhead.SelectedIndex = 0;
+                return;
+            }
+        }
+
+        catch (Exception ex)
+        {
+            //objCommon.DisplayMessage("Exception Occured,Contact To Administrator!!", this);
+            objCommon.DisplayMessage("Data is not in correct format!!", this);
+        }
+    }
+
+
 }
