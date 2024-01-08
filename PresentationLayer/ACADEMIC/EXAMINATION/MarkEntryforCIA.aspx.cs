f53f1bea8e8130734a93c4c66a47d0bad1bd35f3
Prafull Muke <prafull.muke@mastersofterp.com>
2024-01-08 12:48:00 +0530

    [CHERRY-PICK] Adding '13e33982e84e23be63b5b049788ccfd12de2606b' to 'CHERRY_PICK_main_2024-01-11_17.41.32' using the python script version 'v1.0.6'.

--- PresentationLayer/ACADEMIC/EXAMINATION/MarkEntryforCIA.aspx.cs
+++ PresentationLayer/ACADEMIC/EXAMINATION/MarkEntryforCIA.aspx.cs
@@ -1472,11 +1472,13 @@ public partial class ACADEMIC_EXAMINATION_MarkEntryforCIA : System.Web.UI.Page
         ViewState["examName_POP"] = Convert.ToString(lbtn.CommandArgument.Split(',')[5]);
         ViewState["fldname_POP"] = Convert.ToString(lbtn.CommandArgument.Split(',')[3]);
 
-        ViewState["ccode_POP"] = lbl_SubjectName.Text.Split('~')[0];
+        
 
 
         string coursename = objCommon.LookUp("ACD_COURSE", "COURSE_NAME", "COURSENO="+Convert.ToInt32(ViewState["courseNo_POP"]));
+        string ccode = objCommon.LookUp("ACD_COURSE", "CCODE", "COURSENO=" + Convert.ToInt32(ViewState["courseNo_POP"]));
         lbl_SubjectName.Text = coursename.ToString();
+        ViewState["ccode_POP"] = ccode.ToString();
 
         ddlExamPrint.Items.Clear();
         ddlExamPrint.Items.Add(new ListItem("Please Select", "0"));
