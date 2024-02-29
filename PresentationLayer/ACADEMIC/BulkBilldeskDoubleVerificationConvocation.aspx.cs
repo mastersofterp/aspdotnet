7dcf410405fb04ed04b12c7eba98092cd5955501
Sachin Attarkar <sachin.attarkar@mastersofterp.com>
2024-02-29 11:39:53 +0530

    [CHERRY-PICK] Adding 'f5b00581cf3db05d133bd2a23885ddbba3b87775' to 'CHERRY_PICK_main_2024-03-14_15.44.12' using the python script version 'v1.0.8'.

--- PresentationLayer/ACADEMIC/BulkBilldeskDoubleVerificationConvocation.aspx.cs
+++ PresentationLayer/ACADEMIC/BulkBilldeskDoubleVerificationConvocation.aspx.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//======================================================================================
+// PROJECT NAME  : UAIMS                                                                
+// MODULE NAME   : EXAMINATION
+// PAGE NAME     : BILLDESK DOUBLE VERIFICATION FOR CONVOCATION                                                   
+// CREATION DATE : 29-02-2024                                                          
+// CREATED BY    : SACHIN A                          
+// MODIFIED DATE :                                         
+// MODIFIED DESC : ONLINE PAYMENT RECONCILE FOR CONVOCATION REGISTERED STUDENT                                                                 
+//======================================================================================
+
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Data;
@@ -188,80 +198,12 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
                 ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "showPopup();", true);
                 pnlDetails.Visible = true;
                 pnlSelection.Visible = true;
-            }
-            //String data = Convert.ToString("0122|BSABDURCOE|" + ORDERID + "|" + DateTime.Now.ToString("yyyyMMddHHmmss"));
-
-
-
-            // int idno = Convert.ToInt32((myButton.CommandName != string.Empty ? Double.Parse(myButton.CommandName) : 0));
-            // String hash = String.Empty;
-
-            // hash = GetHMACSHA256(data, commonkey);
-
-            // data = data + "|" + hash.ToUpper();
-            // //System.Web.UI.ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AlertBox", "alert('"+data+"');", true);
-            // //string strForm = PreparePOSTForm("https://www.billdesk.com/pgidsk/PGIQueryController", data);
-            // //Page.Controls.Add(new LiteralControl(strForm));
-
-            // ServicePointManager.Expect100Continue = true;
-            // ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            // ServicePointManager.DefaultConnectionLimit = 1000;
-
-            // //var request = (HttpWebRequest)WebRequest.Create("https://www.billdesk.com/pgidsk/PGIQueryController?");
-            // //request.KeepAlive = false;
-            // //request.Timeout = 20000;
-            // //request.ProtocolVersion = HttpVersion.Version10;
-            // //request.ServicePoint.Expect100Continue = false;
-            // //var postData = "msg=" + data;
-
-            // //var data1 = Encoding.ASCII.GetBytes(postData);
-
-            // //request.Method = "POST";
-            // //request.ContentType = "application/x-www-form-urlencoded";
-            // //request.ContentLength = data.Length;
-
-            // //using (var stream = request.GetRequestStream())
-            // //{
-            // //    stream.Write(data1, 0, data1.Length);
-            // //}
-
-            // //var response = (HttpWebResponse)request.GetResponse();
-
-            // //var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            // //System.Web.UI.ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AlertBox", "alert('" + responseString + "');", true);
-
-            // WebRequest request = WebRequest.Create("https://www.billdesk.com/pgidsk/PGIQueryController");
-            // byte[] buffer = System.Text.Encoding.GetEncoding(1252).GetBytes("msg=" + data);
-            // request.Method = WebRequestMethods.Http.Post;
-            // request.ContentType = "application/x-www-form-urlencoded";
-            // Stream requestStream = request.GetRequestStream();
-            // requestStream.Write(buffer, 0, buffer.Length);
-            // requestStream.Close();
-            // Console.Write(buffer.Length);
-            // Console.Write(request.ContentLength);
-            // WebResponse myResponse = request.GetResponse();
-            // string result = "";
-            // StreamReader reader = new StreamReader(myResponse.GetResponseStream());
-            // result = reader.ReadToEnd();
-            // //Label1.Text = result;
-            // //Console.WriteLine(result);
-            //// divFooter.Visible = true;
-            // ViewState["result"] = result;
-            // ViewState["idno"] = idno;
-            // ViewState["ORDERID"] = ORDERID;
-
-            // ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "showPopup();", true);
-            //txnLabel.Text = "1212131";
-            //FillSuccessString(result, idno, ORDERID);
-
-        }
-
+            } 
+        } 
         catch (Exception exp)
         {
-            //Response.Write("Exception " + exp);
-
-        }
-        //}
+            //Response.Write("Exception " + exp); 
+        } 
     }
 
     public string GetHMACSHA256(string text, string key)
@@ -332,8 +274,7 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
             System.Net.HttpWebResponse objResponse = (System.Net.HttpWebResponse)objRequest.GetResponse();//receive the responce from objxmlhttp object 
             using (System.IO.StreamReader sr = new System.IO.StreamReader(objResponse.GetResponseStream()))
             {
-                message = sr.ReadToEnd();
-                //Response.Write(message);
+                message = sr.ReadToEnd(); 
             }
         }
         catch (Exception exception)
@@ -364,10 +305,7 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
     }
 
     public void FillSuccessString(String data, int idno, string ORDERID)
-    {
-        //data = "0130|BSABDURCOE|7227162465649|WCPH1234502060|217509835007|1500.00|CPH|NA|10|INR|DIRECT|NA|NA|00.00|24-06-2022 14:09:12|0300|NA|SATHISH. J|9489563290|AEF|188031601043|972|5649|0|NA|Success|N|NA|0.00|NA|NA|Y|511A4984C8737D9AF18598DCB000E593D26A018443D84EC3D8A6B15FA444A910";
-        //0130|BSABDURCOE|63767719121175|XHD50922370598|235364370158|1000.00|HD5|NA|10|INR|DIRECT|NA|NA|00000000.00|19-12-2022 21:32:12|0300|NA|SURYA HUSSAIN|9884678193|CONV|191291601175|1175|1175|0|NA|NA|Y|NA|0.00|NA|NA|Y|D5A4F7AD2E486181594EC06A064DB240C7EB96B9298C7B7CE6E5D2EFA8C2EC22
-
+    { 
         lblResponse.Text = data;
         pnlSelection.Visible = false;
         pnlDetails.Visible = false;
@@ -379,9 +317,7 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
         lblAmount.Text = listStrLineElements[5].ToString();
         TranDateTime = listStrLineElements[14].ToString();
         TranDate = TranDateTime.Split(' ')[0];
-        ViewState["TranDate"] = TranDateTime.Split(' ')[0];
-
-       // ViewState["Transactionid"] = listStrLineElements[3].ToString();         //APTRANSACTIONID 
+        ViewState["TranDate"] = TranDateTime.Split(' ')[0]; 
 
         ViewState["ApTransactionid"] = listStrLineElements[3].ToString();         //APTRANSACTIONID  
         ViewState["Transactionid"] = listStrLineElements[4].ToString();           //Trnasaction id 
@@ -397,29 +333,7 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
             lblStatus.Text = "Failed";
             btnSubmit.Visible = false;      
             lblStatus.ForeColor = System.Drawing.Color.Red;
-        }
-
-        //string[] segments = data.Split('|');
-        //if (status == "0300" && refundstatus == "NA")
-        //Label_MerchTxnRef.Text = listStrLineElements[3].ToString();
-        // lblEnrollNo.Text = listStrLineElements[2].ToString();
-        //else
-        //{ 
-        //}
-        //foreach (ListViewItem item in lvlist.Items)
-        //{
-        //    Label lborderid = item.FindControl("lblorderId") as Label;
-        //  //  string idno = (item.FindControl("hdid") as HiddenField).Value;
-        //    Label lbamount = item.FindControl("lblamount") as Label;
-        //    Label lbmerchantid = item.FindControl("lblmertransactionid") as Label;
-
-        // ViewState["IDNO"] = idno;
-        // lblamount.Text = listStrLineElements[5].ToString();
-        // lblname.Text = listStrLineElements[17].ToString();
-        // string Enrollno = objCommon.LookUp("ACD_STUDENT", "REGNO", "IDNO= " + idno);
-        //lblEnrollNo.Text = Enrollno.ToString();
-
-        // }
+        } 
     }
     protected void btnCancel_Click(object sender, EventArgs e)
     {
@@ -435,15 +349,14 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
     protected void btnSubmit_Click(object sender, EventArgs e)
     {
         try
-        {
-            //FillSuccessString(ViewState["result"].ToString(),Convert.ToInt32(ViewState["idno"]),ViewState["ORDERID"].ToString());
+        { 
             if (txnLabel.Text == "NA" || lblAmount.Text == "NA" || lblStatus.Text == "NA")
             {
                 objCommon.DisplayMessage(this.Page, "Failed to manage", this.Page);
                  return;
             }
             string Enrollno = string.Empty;
-            Enrollno = objCommon.LookUp("ACD_STUDENT", "REGNO", "IDNO= '" + ViewState["idno"] + "'");
+            Enrollno = objCommon.LookUp("ACD_STUDENT", "REGNO", "IDNO= '" + Convert.ToString(ViewState["idno"]) + "'");
             lblEnrollNo.Text = Enrollno.ToString();
             string Reciept_code = string.Empty;
 
@@ -456,13 +369,14 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
             }
             {
                 int output = 0;
-                // output = (objFees.BULKInsertOnlinePayment_DCR((idno).ToString(), (ViewState["reccode"]).ToString(), ORDERID.ToString, lbmerchantid.ToString, "O", "1", lblamount, "Success", Enrollno.ToString(), lblResponse.ToString()));
-                //output = objFees.BulkInsertOnlinePayment_DCR(ViewState["idno"].ToString(), ViewState["reccode"].ToString(), ViewState["ORDERID"].ToString(), txnLabel.Text, "O", "1", lblAmount.Text, "Success", Enrollno.ToString(), lblResponse.ToString(), Convert.ToInt32(Session["OrgId"]));
-                //output = objFees.BulkInsertOnlinePaymentLog(ViewState["ORDERID"].ToString(), Convert.ToInt32(ViewState["idno"]), "1", Reciept_code, Convert.ToString(ViewState["TranDate"]), txnLabel.Text, "1", lblResponse.ToString(), TranDateTime);
-
+                 
+                string inputDate = Convert.ToString(ViewState["TranDate"]);  
+                DateTime parsedDate = DateTime.ParseExact(inputDate, "dd-MM-yyyy", null);  // Parse the input date using the specified format 
+                string outputDate = parsedDate.ToString("yyyy-MM-dd");                // Format the parsed date to the desired output format
+ 
                 string SP_Name = "PKG_ACAD_CONVOCATION_AUTO_UPDATED_TRANSACTION";
                 string SP_Parameters = "@P_ORDER_ID, @P_IDNO, @P_ISVALID, @P_RECEIPT_CODE, @P_TRANSACTION_DATE, @P_TRANSID, @P_PAY_STATUS, @P_MESSAGE,@P_APTRANSACTIONID,@P_OUTPUT";
-                string Call_Values = "" + ViewState["ORDERID"].ToString() + "," + Convert.ToInt32(ViewState["idno"]) + ",1," + Reciept_code + "," + Convert.ToString(ViewState["TranDate"]) + "," + Convert.ToString(ViewState["Transactionid"]) + ",1," + Convert.ToString(ViewState["result"]) + "," + Convert.ToString(ViewState["ApTransactionid"]) + ",0";
+                string Call_Values = "" + ViewState["ORDERID"].ToString() + "," + Convert.ToInt32(ViewState["idno"]) + ",1," + Reciept_code + "," + outputDate + "," + Convert.ToString(ViewState["Transactionid"]) + ",1," + Convert.ToString(ViewState["result"]) + "," + Convert.ToString(ViewState["ApTransactionid"]) + ",0";
                   
                 string que_out = objCommon.DynamicSPCall_IUD(SP_Name, SP_Parameters, Call_Values, true); 
                // string que_out = AL.DynamicSPCall_IUD(SP_Name, SP_Parameters, Call_Values, true, 2);
@@ -494,110 +408,5 @@ public partial class ACADEMIC_BulkBilldeskDoubleVerificationConvocation : System
         txtprnno.Text = string.Empty;
         ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "closePopup();", true);
     }
-    //public void doubleVerification()
-    //{
-    //    try
-    //    {
-    //        String data = "0122|CRES" + "|" + ViewState["Orderid"] + "|" + DateTime.Now.ToString("yyyyMMddhhmmss");
-    //        //string data = "0122|SVCE2|" + ViewState["Orderid"] + "|SHMP8634441559|993142|1.00|HMP|510372|03|INR|MDDIRECT|02-NA|NA|00000000.00|19-03-2020 10:46:48|0300|NA|BALACHANDAR M|1662|AEF|AEF180301010,58,3,9444208734|1|NA|58|NA|PGS10001-Success|gpOT4FkIAejC";
-    //        string ChecksumKey = "sNPAFD72PKSWXYICHM2SyYgaQ7pF1Xf4";
-    //        String hash = String.Empty;
-    //        hash = GetHMACSHA256(data, ChecksumKey);
-    //        hash = hash.ToUpper();
-    //        string msg = data + "|" + hash;
-
-    //        //TO ENABLE SECURE CONNECTION SSL/TLS
-    //        ServicePointManager.Expect100Continue = true;
-    //        ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-
-    //        var request = (HttpWebRequest)WebRequest.Create("https://www.billdesk.com/pgidsk/PGIQueryController?msg=" + msg);
-    //        var response = (HttpWebResponse)request.GetResponse();
-    //        Stream dataStream = response.GetResponseStream();
-    //        StreamReader reader = new StreamReader(dataStream);
-    //        string strResponse = reader.ReadToEnd();
-    //        //lblNote1.Text = strResponse;
-    //        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["UAIMS"].ToString());
-    //        if (con.State == ConnectionState.Open)
-    //        {
-    //            con.Close();
-    //        }
-    //        con.Open();
-    //        string res = "RD";
-    //        SqlCommand cmd = new SqlCommand("Insert into Test (Name,Response) values ('" + strResponse.ToString() + "','" + res + "')", con);
-    //        cmd.Connection = con;
-    //        cmd.ExecuteNonQuery();
-    //        con.Close();
-    //        //lblNote1.Text = string.Empty;
-    //        string[] repoarray;
-    //        repoarray = strResponse.Split('|');
-
-    //        string authstatus = repoarray[15].ToString();
-    //        string txnid1 = repoarray[2].ToString();
-    //        string amount1 = repoarray[5].ToString();
-    //        string apitransid = repoarray[3].ToString();
-    //        string BankReferenceNo = repoarray[4].ToString();
-    //        string TxnType = repoarray[8].ToString();
-    //        string receipt = repoarray[19].ToString();
-    //        //string status_msg = repoarray[25].ToString();
-
-    //        string rec_code = objCommon.LookUp("ACD_DCR", "RECIEPT_CODE", "ORDER_ID = '" + ViewState["Orderid"] + "'");
-    //        //string status = repoarray[15].ToString();
-    //        //string status_msg = repoarray[25].ToString();
-    //        //string msgR = repoarray[32].ToString();
-    //        string txnMessage = string.Empty;
-    //        string txnStatus = string.Empty;
-    //        #region Get Transaction Details
-    //        if (authstatus == "0300")
-    //        {
-    //            txnMessage = "Successful Transaction";
-    //            txnStatus = "Success";
-    //        }
-    //        else if (authstatus == "0399")
-    //        {
-    //            txnMessage = "Cancel Transaction";
-    //            txnStatus = "Invalid Authentication at Bank";
-    //        }
-    //        else if (authstatus == "NA")
-    //        {
-    //            txnMessage = "Cancel Transaction";
-    //            txnStatus = "Invalid Input in the Request Message";
-    //        }
-    //        else if (authstatus == "0002")
-    //        {
-    //            txnMessage = "Cancel Transaction";
-    //            txnStatus = "BillDesk is waiting for Response from Bank";
-    //        }
-    //        else if (authstatus == "0001")
-    //        {
-    //            txnMessage = "Cancel Transaction";
-    //            txnStatus = "Error at BillDesk";
-    //        }
-    //        else
-    //        {
-    //            txnMessage = "Something went wrong. Try Again!.";
-    //            txnStatus = "Payment Faild";
-    //        }
-    //        #endregion
-
-    //        FeeCollectionController objFeesCnt = new FeeCollectionController();
-    //        //if (authstatus == "0300")
-    //        //{
-    //        int retval = 0;
-    //        //retval = objFees.BulkInsertOnlinePayment_DCR(txnid1, Convert.ToInt32(ViewState["idno"]), "1", receipt, Convert.ToString(ViewState["TranDate"]), txnStatus, "1", txnMessage, TranDateTime);
-
-    //        //if (rec_code == "PRF")
-    //        //{
-    //        //    retval = objFeesCnt.OnlinePaymentUpdation(apitransid, txnid1, amount1, authstatus, txnStatus, TxnType, txnMessage, BankReferenceNo, 1);
-
-    //        //}
-    //        //else if (rec_code == "RF")
-    //        //{
-    //        //    retval = objFeesCnt.OnlinePaymentUpdation(apitransid, txnid1, amount1, authstatus, txnStatus, TxnType, txnMessage, BankReferenceNo, 2);
-
-    //        //}
-
-
-    //    }
-    //    catch { }
-    //}
+    
 }
