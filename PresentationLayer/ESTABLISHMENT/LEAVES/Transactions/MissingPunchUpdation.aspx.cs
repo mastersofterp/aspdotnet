dc08c82fb38a1359dd8c84a6fd71a28e83648288
Piyush <piyush.thakre1664>
2024-03-08 12:27:47 -0800

    [CHERRY-PICK] Adding '7b964adac24d9a2d99a0090eb353e2cd18c0481c' to 'CHERRY_PICK_main_2024-03-14_15.44.12' using the python script version 'v1.0.8'.

--- PresentationLayer/ESTABLISHMENT/LEAVES/Transactions/MissingPunchUpdation.aspx.cs
+++ PresentationLayer/ESTABLISHMENT/LEAVES/Transactions/MissingPunchUpdation.aspx.cs
@@ -74,24 +74,32 @@ public partial class ESTABLISHMENT_LEAVES_Transactions_MissingPunchUpdation : Sy
     {
         try
         {
-            objLM.DATE = Convert.ToDateTime(txtWDate.Text);
-            objLM.REASON = txtReason.Text.ToString();
-            objLM.IDNO = Convert.ToInt32(Session["idno"]);
-            objLM.INTIME = txtInTime.Text.ToString();
-            objLM.OUTTIME = txtOutTime.Text.ToString();
-            objLM.CreatedBy = Convert.ToInt32(Session["userno"]);
-            objLM.ModifiedBy = Convert.ToInt32(Session["userno"]);
-            objLM.COLLEGE_CODE = Session["colcode"].ToString();
-            int MissPunchNo = 0;
-
-            CustomStatus cs = (CustomStatus)objLC.AddMissPunchUpdation(objLM, MissPunchNo);
-
-            if (cs.Equals(CustomStatus.RecordUpdated))
+            if (Convert.ToBoolean(ViewState["ValidTime"]) == true)
             {
-                objCommon.DisplayMessage("Record Saved Successfully...", this.Page);
-                BindMissPunchListView();
-                Clear();
+                Boolean chkv = CheckValid();
+                if (chkv == true)
+                {
+                    objLM.DATE = Convert.ToDateTime(txtWDate.Text);
+                    objLM.REASON = txtReason.Text.ToString();
+                    objLM.IDNO = Convert.ToInt32(Session["idno"]);
+                    objLM.INTIME = txtInTime.Text.ToString();
+                    objLM.OUTTIME = txtOutTime.Text.ToString();
+                    objLM.CreatedBy = Convert.ToInt32(Session["userno"]);
+                    objLM.ModifiedBy = Convert.ToInt32(Session["userno"]);
+                    objLM.COLLEGE_CODE = Session["colcode"].ToString();
+                    int MissPunchNo = 0;
+
+                    CustomStatus cs = (CustomStatus)objLC.AddMissPunchUpdation(objLM, MissPunchNo);
+
+                    if (cs.Equals(CustomStatus.RecordUpdated))
+                    {
+                        objCommon.DisplayMessage("Record Saved Successfully...", this.Page);
+                        BindMissPunchListView();
+                        Clear();
+                    }
+                }
             }
+          
         }
         catch (Exception ex)
         {
@@ -143,24 +151,80 @@ public partial class ESTABLISHMENT_LEAVES_Transactions_MissingPunchUpdation : Sy
     }
     protected void txtOutTime_TextChanged(object sender, EventArgs e)
     {
-        if (txtInTime.Text.ToString() != string.Empty && txtOutTime.Text.ToString() != string.Empty)
-        {
-            DateTime intime = Convert.ToDateTime(txtInTime.Text);
-            DateTime outtime = Convert.ToDateTime(txtOutTime.Text);
+        //if (Page.IsValid)
+        //{
+        //    if (txtInTime.Text.ToString() != string.Empty && txtOutTime.Text.ToString() != string.Empty)
+        //    {
+        //        DateTime intime = Convert.ToDateTime(txtInTime.Text);
+        //        DateTime outtime = Convert.ToDateTime(txtOutTime.Text);
 
-            if (outtime <= intime)
+        //        if (outtime <= intime)
+        //        {
+        //            objCommon.DisplayMessage("Please Enter Time in 24 hours format...", this.Page);
+        //            btnSave.Enabled = false;
+        //        }
+        //        else
+        //        {
+        //            btnSave.Enabled = true;
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    objCommon.DisplayMessage("Please Enter Valid Time!!", this.Page);
+        //}
+        //btnSave.Enabled = true; 
+
+        if (!string.IsNullOrEmpty(txtInTime.Text) && !string.IsNullOrEmpty(txtOutTime.Text))
+        {
+            DateTime intime, outtime;
+
+            if (DateTime.TryParse(txtInTime.Text, out intime) && DateTime.TryParse(txtOutTime.Text, out outtime))
             {
-                objCommon.DisplayMessage("Please Enter Time in 24 hours format...", this.Page);
-                btnSave.Enabled = false;
+                if (outtime <= intime)
+                {
+                    objCommon.DisplayMessage("Please Enter Time in 24 hours format...", this.Page);
+                    //btnSave.Enabled = false;
+                    ViewState["ValidTime"] = false;
+                    return;
+                }
+                else
+                {
+                    ViewState["ValidTime"] = true;
+                }
             }
             else
             {
-                btnSave.Enabled = true;
+                objCommon.DisplayMessage("Please Enter Valid Time!!", this.Page);
+                //btnSave.Enabled = false;
+                ViewState["ValidTime"] = false;
+                return;
             }
+        }
+        else
+        {
+            objCommon.DisplayMessage("Please Enter Valid Time!!", this.Page);
+            //btnSave.Enabled = false;
+            ViewState["ValidTime"] = false;
+            return;
         }
     }
     protected void txtInTime_TextChanged(object sender, EventArgs e)
     {
         txtOutTime.Text = string.Empty;
+    }
+
+    private Boolean CheckValid()
+    {
+        string WrkDate = txtWDate.Text;
+        DateTime WorkDate = DateTime.Parse(WrkDate);
+        string WRKDATE = WorkDate.ToString("yyyy/MM/dd");
+        string rec = objCommon.LookUp("ESTB_MISSING_PUNCH_UPD_REQ", "MPNO", "WRKDATE= '" + WRKDATE + "' And EMPNO = '" + Convert.ToInt32(Session["idno"]) + "'");
+        if (rec != string.Empty)
+        {
+            objCommon.DisplayMessage("Record Already Exist!", this.Page);
+            return false;
+        }
+        return true;
     }
 }
