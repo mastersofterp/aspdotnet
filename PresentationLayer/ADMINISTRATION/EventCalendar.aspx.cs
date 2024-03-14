9754ac16e837a2a40447bd9f4207723673bbe157
kajal jaiswal <kajal.jaiswal@mastersofterp.com>
2024-03-14 11:11:02 +0530

    [CHERRY-PICK] Adding '474d67e33f0d8fefad8e3e6d0cc697a68ace4ced' to 'CHERRY_PICK_main_2024-04-05_16.27.21' using the python script version 'v1.0.8'.

---
+++ PresentationLayer/ADMINISTRATION/EventCalendar.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IITMS;
+using IITMS.UAIMS;
+using IITMS.UAIMS.BusinessLayer.BusinessLogic;
+
+public partial class ADMINISTRATION_EventCalendar : System.Web.UI.Page
+{
+    Common objCommon = new Common();
+    UAIMS_Common objUaimsCommon = new UAIMS_Common();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!Page.IsPostBack)
+            {
+                // Check User Session
+                if (Session["userno"] == null || Session["username"] == null ||
+                    Session["usertype"] == null || Session["userfullname"] == null)
+                {
+                    Response.Redirect("~/default.aspx");
+                }
+                else
+                {
+                   
+                }
+                
+            }
+           
+        }
+        catch (Exception ex)
+        {
+            if (Convert.ToBoolean(Session["error"]) == true)
+                objUaimsCommon.ShowError(Page, "EventCalender.Page_Load() --> " + ex.Message + " " + ex.StackTrace);
+            else
+                objUaimsCommon.ShowError(Page, "Server Unavailable.");
+        }
+    }
+
+   
+}
+
