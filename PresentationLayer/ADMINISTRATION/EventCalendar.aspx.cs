0df88928100a1641025fceaf9fce196a8971481a
kajal jaiswal <kajal.jaiswal@mastersofterp.com>
2024-03-18 14:54:30 +0530

    [CHERRY-PICK] Adding '644957ce6b275c049e73f9d16cdf4acf8a093c64' to 'CHERRY_PICK_main_2024-04-05_16.27.21' using the python script version 'v1.0.8'.

--- PresentationLayer/ADMINISTRATION/EventCalendar.aspx.cs
+++ PresentationLayer/ADMINISTRATION/EventCalendar.aspx.cs
@@ -1,49 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Script.Serialization;
-using System.Web.Services;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using IITMS;
-using IITMS.UAIMS;
-using IITMS.UAIMS.BusinessLayer.BusinessLogic;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Script.Serialization;
+    using System.Web.Services;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using IITMS;
+    using IITMS.UAIMS;
+    using IITMS.UAIMS.BusinessLayer.BusinessLogic;
 
-public partial class ADMINISTRATION_EventCalendar : System.Web.UI.Page
-{
-    Common objCommon = new Common();
-    UAIMS_Common objUaimsCommon = new UAIMS_Common();
-
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class ADMINISTRATION_EventCalendar : System.Web.UI.Page
     {
-        try
+        Common objCommon = new Common();
+        UAIMS_Common objUaimsCommon = new UAIMS_Common();
+
+        protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Page.IsPostBack)
+            try
             {
-                // Check User Session
-                if (Session["userno"] == null || Session["username"] == null ||
-                    Session["usertype"] == null || Session["userfullname"] == null)
+                if (!Page.IsPostBack)
                 {
-                    Response.Redirect("~/default.aspx");
-                }
-                else
-                {
+                    // Check User Session
+                    if (Session["userno"] == null || Session["username"] == null ||
+                        Session["usertype"] == null || Session["userfullname"] == null)
+                    {
+                        Response.Redirect("~/default.aspx");
+                    }
+                    else
+                    {
                    
-                }
+                    }
                 
-            }
+                }
            
+            }
+            catch (Exception ex)
+            {
+                if (Convert.ToBoolean(Session["error"]) == true)
+                    objUaimsCommon.ShowError(Page, "EventCalender.Page_Load() --> " + ex.Message + " " + ex.StackTrace);
+                else
+                    objUaimsCommon.ShowError(Page, "Server Unavailable.");
+            }
         }
-        catch (Exception ex)
-        {
-            if (Convert.ToBoolean(Session["error"]) == true)
-                objUaimsCommon.ShowError(Page, "EventCalender.Page_Load() --> " + ex.Message + " " + ex.StackTrace);
-            else
-                objUaimsCommon.ShowError(Page, "Server Unavailable.");
-        }
-    }
 
    
-}
+    }
 
--- PresentationLayer/WebServices/EventCalendar.asmx
+++ PresentationLayer/WebServices/EventCalendar.asmx
@@ -1,40 +1,40 @@
-﻿<%@ WebService Language="C#" Class="EventCalendar" %>
+﻿    <%@ WebService Language="C#" Class="EventCalendar" %>
 
-using System;
-using System.Data;
-using System.Web;
-using System.Web.Services;
-using System.Web.Services.Protocols;
-using System.Collections.Generic;
-using IITMS.UAIMS.BusinessLayer.BusinessLogic;
-using IITMS.UAIMS;
-using Newtonsoft.Json;
-using BusinessLogicLayer.BusinessLogic;
+    using System;
+    using System.Data;
+    using System.Web;
+    using System.Web.Services;
+    using System.Web.Services.Protocols;
+    using System.Collections.Generic;
+    using IITMS.UAIMS.BusinessLayer.BusinessLogic;
+    using IITMS.UAIMS;
+    using Newtonsoft.Json;
+    using BusinessLogicLayer.BusinessLogic;
 
-[WebService(Namespace = "http://tempuri.org/")]
-[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-[System.Web.Script.Services.ScriptService]
-public class EventCalendar  : System.Web.Services.WebService {
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class EventCalendar  : System.Web.Services.WebService {
     
-    EventCalendarController objevent = new EventCalendarController();
-    Common objCommon = new Common();
+        EventCalendarController objevent = new EventCalendarController();
+        Common objCommon = new Common();
    
 
-    [WebMethod(EnableSession = true)]
-    public string SpecialEvent()
-    {
-        var jsonstring = string.Empty;
-        DataSet ds = new DataSet();
-        int UANO = Convert.ToInt32(Session["userno"].ToString());
-        ds = objevent.GetAllEventList(UANO);
-
-        if (ds.Tables[0].Rows.Count > 0)
+        [WebMethod(EnableSession = true)]
+        public string SpecialEvent()
         {
-            jsonstring = JsonConvert.SerializeObject(ds.Tables[0]);
+            var jsonstring = string.Empty;
+            DataSet ds = new DataSet();
+            int UANO = Convert.ToInt32(Session["userno"].ToString());
+            ds = objevent.GetAllEventList(UANO);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                jsonstring = JsonConvert.SerializeObject(ds.Tables[0]);
+            }
+            return jsonstring;
         }
-        return jsonstring;
-    }
     
-}
+    }
 
