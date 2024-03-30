dcdfee668d923773e4ec78c69e60ebb9d48318f7
shailendra.kuchanwar1473 <shailendra.kuchanwar@mastersofterp.com>
2024-03-30 10:25:45 +0530

    [BUGFIX] [57025] Organization structure_define college-reg.
    Resolved the issues.

--- PresentationLayer/RFC_CONFIG/Masters/OrgStructure.aspx.cs
+++ PresentationLayer/RFC_CONFIG/Masters/OrgStructure.aspx.cs
@@ -544,14 +544,9 @@ public partial class RFC_CONFIG_Masters_OrgStructure : System.Web.UI.Page
                 {
                     byte[] imgdata = System.IO.File.ReadAllBytes(System.Web.HttpContext.Current.Server.MapPath("~/Images/default-fileupload.png"));
                     ds.Tables[0].Rows[i]["COE_Sign"] = imgdata;
-                }
-
-                if (DBNull.Value.Equals(ds.Tables[0].Rows[i]["Logo"]))
-                {
-                    byte[] imgdata = System.IO.File.ReadAllBytes(System.Web.HttpContext.Current.Server.MapPath("~/Images/default-fileupload.png"));
-                    ds.Tables[0].Rows[i]["Logo"] = imgdata;
-                }
-            }
+					ds.Tables[0].Rows[i]["Logo"] = imgdata;
+				}
+            };
            
             if (ds != null && ds.Tables[0].Rows.Count > 0)
             {
