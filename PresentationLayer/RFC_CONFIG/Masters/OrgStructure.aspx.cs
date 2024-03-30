3098856f74573e66d481d716e134da8c67e83aea
shailendra.kuchanwar1473 <shailendra.kuchanwar@mastersofterp.com>
2024-03-30 11:44:18 +0530

    [CHERRY-PICK] Adding 'b7c4546c260a13d0abe8afb52e1b033640eb8268' to 'CHERRY_PICK_main_2024-04-05_16.27.21' using the python script version 'v1.0.8'.

--- PresentationLayer/RFC_CONFIG/Masters/OrgStructure.aspx.cs
+++ PresentationLayer/RFC_CONFIG/Masters/OrgStructure.aspx.cs
@@ -544,9 +544,14 @@ public partial class RFC_CONFIG_Masters_OrgStructure : System.Web.UI.Page
                 {
                     byte[] imgdata = System.IO.File.ReadAllBytes(System.Web.HttpContext.Current.Server.MapPath("~/Images/default-fileupload.png"));
                     ds.Tables[0].Rows[i]["COE_Sign"] = imgdata;
-					ds.Tables[0].Rows[i]["Logo"] = imgdata;
-				}
-            };
+                }
+
+                if (DBNull.Value.Equals(ds.Tables[0].Rows[i]["Logo"]))
+                {
+                    byte[] imgdata = System.IO.File.ReadAllBytes(System.Web.HttpContext.Current.Server.MapPath("~/Images/default-fileupload.png"));
+                    ds.Tables[0].Rows[i]["Logo"] = imgdata;
+                }
+            }
            
             if (ds != null && ds.Tables[0].Rows.Count > 0)
             {
